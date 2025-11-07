509607417d7c83f79a1db337572580fea0225b95
Dylan P <dylan.m.priebe@gmail.com>
2025-11-06 18:41:29 -0600

    initial commit

---
+++ backend/backend/src/test/java/cc/dylanpriebe/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.dylanpriebe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
